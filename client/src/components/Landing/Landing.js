c41073e925256303eaec85c6bb7d515b3a23204d
krishnamoorthy <krishnamoorthy@wealwin.com>
2025-12-01 18:59:38 +0530

    bugfix

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -8,7 +8,7 @@ const Landing = () => {
         <div className="landing__container">
             <div className="landing__header__container">
                 <div className="landing__header">
-                    <h3 className="landing__header__discount">UP TO 15% DISCOUNT</h3>
+                    <h3 className="landing__header__discount">UP TO 45% DISCOUNT</h3>
                     <h1 className="landing__header__main">Checkout The Best Fashion Style</h1>
                     <Link to="/shop">
                         <Button variant='outlined' sx={[ {width: '190px', height: '50px', borderRadius: '20px' , fontWeight: '700', backgroundColor: 'none', borderColor: 'black', color: 'black' }, {'&:hover': {  backgroundColor: "black" , color: "#FFE26E", borderColor: 'black'}}]}>SHOP NOW</Button>
@@ -16,7 +16,7 @@ const Landing = () => {
                 </div>
             </div>
             <div className="landing__image__container">
-                <img className="landing__image" src={land} alt=""/>
+                <img className="landing__image" src={land} alt=""/> 
             </div>
         </div>
      );
